c52df10316ef849005d2d1bfc8e4b0b906adb7eb
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-28 08:35:04 +0100

    Commit

---
+++ Check the remainder of division/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 523134;
+        int y = 325423;
+        int remainder = x % y;
+
+        if (remainder == 0)
+        {
+            Console.WriteLine("No remainder");
+        }
+        else if (remainder > 1000)
+        {
+            Console.WriteLine("Remainder is above 1000");
+        }
+        else
+        {
+            Console.WriteLine("Remainder is below 1000");
+        }
+    }
+}
+
